69e4c50810ed7a0cd778a30d366c3b63eb4e2aba
s.akhmedoff <akhmedov.sadik@gmail.com>
2018-06-30 02:14:56 +0500

    begin

---
+++ account.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name          Account
+// @author        s.akhmedoff (S A)
+// @description   Some Simple Account Class (Practice-mode)
+// ==/UserScript==
+//
+
+class Account {
+    constructor (params) {
+        this.params = params;
+    }  
+
+    getInstance() {
+        return this;
+    }
+
+    
+}
+
